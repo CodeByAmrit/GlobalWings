28e374eb3dab01e28de98047f6caa565ebeaf51e
Amrit <35948782+CodeByAmrit@users.noreply.github.com>
2024-09-01 12:32:00 +0530

    Favicon Added

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 const aircraftRoutes = require('./routes/aircraftRoutes');
-
+const favicon = require('serve-favicon');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api', aircraftRoutes);
 app.use(express.static(path.join(__dirname, 'public')))
+// Serve favicon from the public directory
+app.use(favicon(path.join(__dirname, 'public', 'logos', 'favicon.ico')));
 
 app.listen(port, (err) => {
     if (err){
