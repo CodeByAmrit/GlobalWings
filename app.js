5ee3dc4afdd528f88de5bf720fae3208899973a3
Amrit <35948782+CodeByAmrit@users.noreply.github.com>
2024-09-01 14:35:11 +0530

    security measure like helment and x-powered by and ejs

--- app.js
+++ app.js
@@ -1,21 +1,60 @@
 const express = require('express');
 const path = require('path');
-const aircraftRoutes = require('./routes/aircraftRoutes');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const favicon = require('serve-favicon');
+const aircraftRoutes = require('./routes/aircraftRoutes');
+const pagesRoutes = require('./routes/pagesRoutes');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.disable('x-powered-by');
+
+// Middleware: Set security headers using helmet
+app.use(helmet());
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"],
+            connectSrc: ["'self'", "blob:"],
+            imgSrc: ["'self'", "blob:", "data:"],
+            mediaSrc: ["'self'", "blob:"],
+        }
+    }
+}));
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+
+// Middleware: Logging HTTP requests
+app.use(morgan('combined'));
+
+// Middleware: Parse JSON payloads
 app.use(express.json());
-app.use('/api', aircraftRoutes);
-app.use(express.static(path.join(__dirname, 'public')))
-// Serve favicon from the public directory
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve favicon
 app.use(favicon(path.join(__dirname, 'public', 'logos', 'favicon.ico')));
 
+// Routes
+app.use('/api', aircraftRoutes);
+
+// Routes pages
+app.use('/', pagesRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+});
+
+// Start the server
 app.listen(port, (err) => {
-    if (err){
-        console.log(err);
-    }
-    else{
-        console.log(`Server is running on port http://localhost:${port}`);
+    if (err) {
+        console.error('Failed to start the server:', err);
+    } else {
+        console.log(`Server is running on http://localhost:${port}`);
     }
 });
